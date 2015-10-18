5755be6dfc84093df8179b4b24bc81d5ff369acb
John Murray <heyjohnmurray@us.ibm.com>
2015-10-17 22:12:36 -0500

    add initial gulp plugins that i think i will need

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+// http://www.browsersync.io/docs/gulp/
