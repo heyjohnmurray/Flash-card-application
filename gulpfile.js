13974c9a8818c0b4162f1a879a611a69ec0eecf7
John Murray <heyjohnmurray@us.ibm.com>
2015-10-18 00:45:44 -0500

    add auto prefixing

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,10 @@ var paths = {
 gulp.task('sass', function() {
   return gulp.src(paths.styles.src + '**/*.scss')
     .pipe(sourcemaps.init())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.styles.dest));
