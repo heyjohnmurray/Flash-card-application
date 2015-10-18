c2812c466fac49848a2ad2681d85aef52203a2f9
John Murray <heyjohnmurray@us.ibm.com>
2015-10-17 23:49:29 -0500

    set up file path variables

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,32 @@ var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
+var basePaths = {
+  src: 'app/src/',
+  dest: 'app/dist/'
+};
+
+var paths = {
+  styles: {
+    src: basePaths.src + 'scss/',
+    dest: basePaths.dest + 'css'
+  },
+
+  scripts: {
+    src: basePaths.src + 'js/',
+    dest: basePaths.dest + 'js/'
+  }
+};
+
 gulp.task('sass', function() {
-  return gulp.sass() // finish doing output for this task by adding src and dest paths
+  return gulp.src('');
 });
 
 gulp.task('js', function() {
+
+});
+
+gulp.task('watch', function(){
 
 });
 
@@ -16,4 +37,6 @@ gulp.task('default', function() {
   // place code for your default task here
 });
 
+
 // http://www.browsersync.io/docs/gulp/
+http://www.mikestreety.co.uk/blog/an-advanced-gulpjs-file
